b30b3dee0ad2e9d2dbf152a60be85834a75b1165
wanglipeng <wanglipeng@huayun.com>
2023-11-14 11:00:32 +0800

    add uprobe attach pid

---
+++ examples/c/uprobe_attach_trigger.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+/* It's a global function to make sure compiler doesn't inline it. */
+int uprobed_add(int a, int b)
+{
+    return a + b;
+}
+
+int uprobed_sub(int a, int b)
+{
+    return a - b;
+}
+
+int main(int argc, char * argv[])
+{
+    int i = 0;
+
+    for (i = 0;; i++) {
+                /* trigger our BPF programs */
+                uprobed_add(i, i + 1);
+                uprobed_sub(i * i, i);
+                sleep(1);
+        }
+}
